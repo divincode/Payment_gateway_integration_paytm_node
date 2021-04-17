5465b8a1e82945a09fd9ddbecc06d70255a5bf04
divincode <devadiga.1@iitj.ac.in>
2021-04-18 01:45:56 +0530

    Changed for actual money

--- index.js
+++ index.js
@@ -45,8 +45,8 @@ if(!paymentDetails.amount || !paymentDetails.customerId || !paymentDetails.custo
 
 
     checksum_lib.genchecksum(params, config.PaytmConfig.key, function (err, checksum) {
-        var txn_url = "https://securegw-stage.paytm.in/theia/processTransaction"; // for staging
-        // var txn_url = "https://securegw.paytm.in/theia/processTransaction"; // for production
+     //   var txn_url = "https://securegw-stage.paytm.in/theia/processTransaction"; // for staging
+         var txn_url = "https://securegw.paytm.in/theia/processTransaction"; // for production
 
         var form_fields = "";
         for (var x in params) {
--- paytm/config.js
+++ paytm/config.js
@@ -1,6 +1,6 @@
 var PaytmConfig = {
-  mid: "gxvYqG60728640993682",
-  key: "Pr3JMlZdWQkaRzwr",
-  website: "WEBSTAGING",
+  mid: "yknahn59910614974454",
+  key: "H3opQDVlHdi4d&cw",
+  website: "DEFAULT",
 };
 module.exports.PaytmConfig = PaytmConfig;
